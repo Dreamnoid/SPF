ecacc97755771280e2cb40ac0f45e2ccc2f9324a
Dreamnoid <adrien.debesson@gmail.com>
2024-01-03 21:50:37 +0100

    Update Linux build scripts

--- SPF/compile.sh
+++ SPF/compile.sh
@@ -1,2 +1,3 @@
+mkdir ../bin/
 g++ -c src/*.cpp -llibsdl2 -Iinclude/ -I/usr/include/SDL2 -I/usr/include/bullet -llibbullet -std=c++17 -fpermissive -fPIC -static
-g++ -shared -o SPF.so *.o
+g++ -shared -o ../bin/SPF.so *.o
---
+++ SPF/install_libs.sh
@@ -0,0 +1,4 @@
+apt install libsdl2-2.0.0
+apt install libsdl2-mixer-dev
+apt install libbullet-dev
+apt install libglm-dev
