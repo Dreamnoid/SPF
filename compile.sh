8546413f48139ee77aab3c95900eff10b379cc83
Dreamnoid <adrien.debesson@gmail.com>
2024-01-03 22:02:33 +0100

    Linux again

--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
 mkdir bin/
-g++ -c SPF/src/*.cpp -llibsdl2 -Iinclude/ -I/usr/include/SDL2 -I/usr/include/bullet -llibbullet -std=c++17 -fpermissive -fPIC -static
+cd SPF
+g++ -c src/*.cpp -llibsdl2 -Iinclude/ -I/usr/include/SDL2 -I/usr/include/bullet -llibbullet -std=c++17 -fpermissive -fPIC -static
+cd ..
 g++ -shared -o bin/SPF.so SPF/*.o
 dotnet build SPFSharp/SPFSharp_linux.csproj -f net7.0
