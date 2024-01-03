a7e33c65c5c1b7030a7e34677bbc9b9e3a0dc364
Dreamnoid <adrien.debesson@gmail.com>
2024-01-03 23:16:38 +0100

    Linux: fix link arguments

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 mkdir bin/
-g++ -c SPF/src/*.cpp -llibsdl2 -Iinclude/ -I/usr/include/SDL2 -I/usr/include/bullet -llibbullet -std=c++17 -fpermissive -fPIC -static
-g++ -shared -o bin/SPF.so SPF/*.o
+g++ -c SPF/src/*.cpp -Iinclude/ -I/usr/include/SDL2 -I/usr/include/bullet -std=c++17 -fpermissive -fPIC -static
+g++ -shared -o bin/SPF.so SPF/*.o -lSDL2 -lSDL2_mixer -llibbullet
 dotnet build SPFSharp/SPFSharp_linux.csproj -f net7.0
