e68665bb396a9490711e7e2d7f5153959aaec561
Dreamnoid <adrien.debesson@gmail.com>
2024-10-30 19:09:18 +0100

    Remove Bullet Physics

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 mkdir bin/
 cd SPF
-g++ -c src/*.cpp -Iinclude/ -I/usr/include/SDL2 -I/usr/include/bullet -std=c++17 -fpermissive -fPIC -static
+g++ -c src/*.cpp -Iinclude/ -I/usr/include/SDL2 -std=c++17 -fpermissive -fPIC -static
 cd ..
-g++ -shared -o bin/SPF.so SPF/*.o -lSDL2 -lSDL2_mixer -llibbullet -rpath,'$ORIGIN'/lib
+g++ -shared -o bin/SPF.so SPF/*.o -lSDL2 -rpath,'$ORIGIN'/lib
 dotnet build SPFSharp/SPFSharp_linux.csproj -f net7.0
--- install_libs.sh
+++ install_libs.sh
@@ -1,5 +1,3 @@
 apt install libsdl2-2.0.0
 apt install libsdl2-dev
-apt install libsdl2-mixer-dev
-apt install libbullet-dev
 apt install libglm-dev
