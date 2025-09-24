bb99f6b768ce08deed840a3cdde8721609e2fed0
Dreamnoid <adrien.debesson@gmail.com>
2025-09-24 19:15:57 +0200

    Fix Linux build

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@ mkdir bin/
 cd SPF
 g++ -c src/*.cpp -Iinclude/ -I/usr/include/SDL2 -std=c++17 -fpermissive -fPIC -static
 cd ..
-g++ -shared -o bin/SPF.so SPF/*.o -lSDL2 -rpath,'$ORIGIN'/lib
+g++ -shared -o bin/SPF.so SPF/*.o -lSDL2 -lGL -rpath,'$ORIGIN'/lib
 dotnet build SPFSharp/SPFSharp_linux.csproj -f net7.0
